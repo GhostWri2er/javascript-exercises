ad01f5b919d0b9990b21d2d180cfe26773ba0d60
Ivan Mironovich <ivanmironovich@vk.com>
2025-11-02 12:41:58 +0500

    Rock and a4

--- sript.js
+++ sript.js
@@ -1,26 +1,95 @@
-function add7 (number) {
-    return number + 7
-}
-
-console.log(add7(10))
-
-function multiply (a, b) {
-    return a * b
-}
-
-console.log(multiply(3, 2))
-
-function capitalize (str) {
-   let substr = str.toLowerCase()
-    console.log(substr)
-  return  substr[0].toUpperCase() + substr.slice(1)
+function getComputerChoice () {
+    
+    let choise = Math.floor(Math.random()*3)
+    
+    if (choise === 0) {
+        return 'Камень'
+    } else if (choise === 1) {
+        return 'Ножницы'
+    } else {
+        return 'Бумага'
+    }
     
 }
 
-console.log(capitalize('aaASDSDS'))
+getComputerChoice(Math.round(Math.random()))
 
-function lastLetter (str) {
-    return str.slice(-1)
+for(let i = 0; i <= 3; i++) {
+    let choiseHuman = prompt('Камень, ножницы, бумага?').toLowerCase()
+    getHumanChoice(choiseHuman);
 }
 
-console.log(lastLetter('12312asdsa'))
+
+let counterComp = 0;
+let counterHuman = 0;
+
+
+function getHumanChoice (choiseHuman, counterComp, counterHuman) {
+    let computerChoise = getComputerChoice().toLowerCase()
+    
+    if (choiseHuman === computerChoise) {
+        console.log('Ничья')
+        return 'Ничья'
+    } else if (choiseHuman === 'камень' && computerChoise === 'бумага') {
+        console.log('Вы проиграли')
+        counterComp++
+        console.log('Счет: у вас: ' + counterHuman + 'У компа: ' + counterComp);
+        
+        if(counterComp === 3) console.log('Вы проиграли счет: ' + counterComp + ':' + counterHuman);
+        counterComp = 0;
+        counterHuman = 0;
+         
+        return  'Вы проиграли'
+    } else if (choiseHuman === 'бумага' && computerChoise === 'ножницы') {
+        console.log('Вы проиграли')
+        counterComp++
+        console.log('Счет: у вас: ' + counterHuman + 'У компа: ' + counterComp);
+        
+       if(counterComp === 3) console.log('Вы проиграли счет: ' + counterComp + ':' + counterHuman)
+        counterComp = 0;
+        counterHuman = 0;
+        return 'Вы проиграли'
+    } else if (choiseHuman === 'ножницы' && computerChoise === 'камень') {
+        console.log('Вы проиграли')
+        counterComp++
+        console.log('Счет: у вас: ' + counterHuman + 'У компа: ' + counterComp);
+        
+        if(counterComp === 3) console.log('Вы проиграли счет: ' + counterComp + ':' + counterHuman);
+        counterComp = 0;
+        counterHuman = 0;
+        return 'Вы проиграли'
+    }  else if (choiseHuman === 'бумага' && computerChoise === 'камень') {
+        console.log('Вы выиграли')
+        counterHuman++
+        console.log('Счет: у вас: ' + counterHuman + 'У компа: ' + counterComp);
+        
+        if(counterComp === 3) console.log('Вы выиграли счет: ' + counterHuman + ':' + counterComp);
+        counterComp = 0;
+        counterHuman = 0;
+        return  'Вы выиграли'
+    } else if (choiseHuman === 'ножницы' && computerChoise === 'бумага') {
+        console.log('Вы выиграли')
+        counterHuman++
+        console.log('Счет: у вас: ' + counterHuman + 'У компа: ' + counterComp);
+        
+        if(counterComp === 3) console.log('Вы выиграли счет: ' + counterHuman + ':' + counterComp);
+        counterComp = 0;
+        counterHuman = 0;
+        return 'Вы выиграли'
+    } else if (choiseHuman === 'камень' && computerChoise === 'ножницы') {
+        console.log('Вы выиграли')
+        counterHuman++
+        console.log('Счет: у вас: ' + counterHuman + 'У компа: ' + counterComp);
+        
+        if(counterComp === 3) console.log('Вы выиграли счет: ' + counterHuman + ':' + counterComp);
+        counterComp = 0;
+        counterHuman = 0;
+        return 'Вы выиграли'
+        
+    }
+    
+}
+    
+    
+
+
